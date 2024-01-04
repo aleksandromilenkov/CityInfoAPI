0c1b3696677d627e82733ded291cfbc5ea006188
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-01-04 23:01:33 +0100

    Controller for Download File

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(
+            FileExtensionContentTypeProvider fileExtensionContentTypeProvider) {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new System.ArgumentNullException(
+                    nameof(fileExtensionContentTypeProvider));
+        }
+
+        [HttpGet("{fileId}")]
+        public IActionResult GetFile(int fileId) {
+            // look up the actual file, depending on the fileId...
+            // demo code
+            var pathToFile = "theory-lectures.pdf";
+
+            // check whether the file exists
+            if (!System.IO.File.Exists(pathToFile)) {
+                return NotFound();
+            }
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(
+               pathToFile, out var contentType)) {
+                contentType = "application/octet-stream"; // if the file type canot be find it will be set as octet-stream
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+        }
+    }
+}
