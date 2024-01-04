3aa098e108800675c213dd3750db2388ef939e7f
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-01-04 22:44:52 +0100

    Creating Controllers, DTOs , Fake Database and Supporting XML outputs

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase {
+        [HttpGet]
+        public IActionResult GetCities() {
+            var cities = new CitiesDataBase();
+            return Ok(cities.Cities);
+        }
+        [HttpGet("{cityId:int}")]
+        public IActionResult GetCity([FromRoute] int cityId) {
+            if (cityId == null) {
+                return BadRequest(ModelState);
+            }
+            var cities = new CitiesDataBase();
+            var city = cities.Cities.Where(c => c.Id == cityId).FirstOrDefault();
+            if (city == null) {
+                return NotFound();
+            }
+            return Ok(city);
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,36 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers {
+    [Route("api/cities/{cityId}/[controller]")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase {
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<PointOfInterestDto>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPointsOfInterest([FromRoute] int cityId) {
+            var cities = new CitiesDataBase();
+            var city = cities.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null) {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{pointOfInterestId}")]
+        [ProducesResponseType(200, Type = typeof(PointOfInterestDto))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPointsOfInterest([FromRoute] int pointOfInterestId, int cityId) {
+            var cities = new CitiesDataBase();
+            var city = cities.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null) {
+                return NotFound();
+            }
+            var point = city.PointsOfInterest.Where(p => p.Id == pointOfInterestId).FirstOrDefault();
+            if (point == null) {
+                return NotFound();
+            }
+            return Ok(point);
+        }
+    }
+}
