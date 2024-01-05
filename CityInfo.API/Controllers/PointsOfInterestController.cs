5dd9839f616d395426ace8bb342ae13e8698e2cb
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-01-05 16:50:53 +0100

    Creating Point Of Interest and Validating its values with DataAnnotations

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -7,7 +7,7 @@ namespace CityInfo.API.Controllers {
     public class PointsOfInterestController : ControllerBase {
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(List<PointOfInterestDto>))]
-        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
         public IActionResult GetPointsOfInterest([FromRoute] int cityId) {
             var cities = new CitiesDataBase();
             var city = cities.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -17,9 +17,9 @@ namespace CityInfo.API.Controllers {
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{pointOfInterestId}")]
+        [HttpGet("{pointOfInterestId}", Name = "GetPointOfInterest")]
         [ProducesResponseType(200, Type = typeof(PointOfInterestDto))]
-        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
         public IActionResult GetPointsOfInterest([FromRoute] int pointOfInterestId, int cityId) {
             var cities = new CitiesDataBase();
             var city = cities.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -31,6 +31,29 @@ namespace CityInfo.API.Controllers {
                 return NotFound();
             }
             return Ok(point);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(PointOfInterestDto))]
+        [ProducesResponseType(400)]
+        public IActionResult CreatePointsOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest) {
+            if (!ModelState.IsValid) {
+                return BadRequest();
+            }
+            var cities = new CitiesDataBase();
+            var city = cities.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null) {
+                return NotFound();
+            }
+            // for now demo calculating the id:
+            var maxPointsOfInterestId = cities.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+            var newPointOfInterest = new PointOfInterestDto() {
+                Id = ++maxPointsOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+            city.PointsOfInterest.Add(newPointOfInterest);
+            return CreatedAtRoute("GetPointOfInterest", new { cityId = cityId, pointOfInterestId = newPointOfInterest.Id }, newPointOfInterest);
         }
     }
 }
