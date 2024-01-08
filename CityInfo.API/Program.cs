ad2aa1480d341c9832ced4388ddd3d15a5ad0d98
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-01-08 22:55:50 +0100

    renaming the connection string of the db

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddControllers(options => {
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 builder.Services.AddScoped<ICitiesDataBase, CitiesDataBase>();
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CityInfoDbConnectionString"));
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
